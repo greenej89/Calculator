43539d2de42d817d1fc26f839a8d1808da92e06b
Jessica Greene <jessica.greene89@gmail.com>
2022-08-05 18:00:52 -0400

    attempted to fix bug

--- script.js
+++ script.js
@@ -1,75 +1,58 @@
 var calculatorStorage = [0];
 var inputValue = "";
 var displayText = "";
+var result = 0;
 
-function concatenateDecimals() {
-    calculatorStorage[calculatorStorage.length-1] = calculatorStorage[calculatorStorage.length-1] + ".";
-    var decimalValue = calculatorStorage[calculatorStorage.legnth-1];
-    console.log(calculatorStorage);
-}
+// function concatenateDecimals() {
+//     calculatorStorage[calculatorStorage.length-1] = calculatorStorage[calculatorStorage.length-1] + ".";
+//     var decimalValue = calculatorStorage[calculatorStorage.legnth-1];
+//     console.log(calculatorStorage);
+// }
 
 function calculate() {
     calculatorStorage.push(inputValue);
     inputValue = "";
-    
-    console.log("calculating")
     calculatorStorage = convertToNumbers(calculatorStorage);
-    console.log(calculatorStorage);
 
     var output = 0;
     if (!isOperator(calculatorStorage[1])) {
         output = calculatorStorage[1];
-        console.log("Output: " + output)
     }
     for (var i = 0; i < calculatorStorage.length; i++) {
         var input = calculatorStorage[i];
-        // if (input === '+') {
-        //     output += calculatorStorage[i+1];
-        //     console.log(output);
-        //     document.querySelector("#display").innerHTML = output;
-        // }
-        // if (input === '-') {
-        //     output -= calculatorStorage[i+1];
-        //     console.log(output);
-        //     document.querySelector("#display").innerHTML = output;
-        // }
-        // if (input === '*') {
-        //     output *= calculatorStorage[i+1];
-        //     console.log(output);
-        //     document.querySelector("#display").innerHTML = output;
-        // }
-        // if (input === '/') {
-        //     output /= calculatorStorage[i+1];
-            // console.log(output);
-            // document.querySelector("#display").innerHTML = output;
-        //}
         output = performOperation(input, output, i);
-        console.log(output);
         document.querySelector("#display").innerHTML = output;
         displayText = output;
     }
     document.querySelector("#display").innerHTML = output;
-    //calculatorStorage = [0];
+    console.log("Output: " + output)
+    result = output
+    console.log("Result: " + result)
 }
 
 function press(key) {
-    displayText = displayText+key;
-    document.querySelector("#display").innerHTML = displayText;
-    inputValue = inputValue + key;
-    console.log(calculatorStorage);
+    if(isOperator(key)) {
+        setOP(key)
+    } else {
+        displayText = displayText.toString() + key;
+        document.querySelector("#display").innerHTML = displayText;
+        inputValue = inputValue + key;
+        console.log("Pressed Number -- Storage:" + calculatorStorage)
+    }
 }
 function setOP(operator) {
     displayText = displayText+operator;
     document.querySelector("#display").innerHTML = displayText;
     calculatorStorage.push(inputValue);
     calculatorStorage.push(operator);
-    console.log(calculatorStorage);
     inputValue = "";
+    console.log("Pressed Operator -- Storage:" + calculatorStorage)
 
 }
 function clr() {
-    displayText = "";
+    displayText ="";
     inputValue = "";
+    result = 0;
     calculatorStorage = [0];
     document.querySelector("#display").innerHTML = 0;
     console.log(calculatorStorage);
@@ -87,7 +70,7 @@ function convertToNumbers(array) {
 }
 
 function isOperator(input) {
-    if (input === '+'|| input === '-'|| input === '*'|| input === '/') {
+    if (input === '+'|| input === '-'|| input === '*'|| input === '/' || input == '.') {
         return true
     } else {
         return false;
